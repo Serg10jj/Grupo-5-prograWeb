6e9c82a4b600d43bc16ff9cdb28289747a60ecb8
ErickSabo <erickand.sabo@gmail.com>
2024-04-09 11:55:17 -0600

    Registro/Login
    Se crearon los componentes consulta, home, libros, login, perfil y registro.
    
    Se trabajo mas que todo en los componentes login y registro, los cuales se comunican con el back-end y mandan los datos a la base de datos, en el componente home solo se creo un "navegador" para poder llegar a los otros componentes.
    
    Planteamos la pagina para que se inicie en el login, si uno no cuneta con una cuneta se dirige a registro y la crea, al crearla lo redirecciona a login y se loguea, al loguearse lo dirige al home.

---
+++ front-end/src/components/consulta/consulta.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Consulta = () => {
+   
+   return (
+      <div>
+      <a href="/home">Home</a>
+      <h1>Consulta</h1>
+      </div>
+   );
+}
+
+Consulta.propTypes = {};
+
+Consulta.defaultProps = {};
+
+export default Consulta;
---
+++ front-end/src/components/libros/libros.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+
+const Libros = () => {
+
+   return (
+      <div>
+      <a href="/home">Home</a>
+      <h1>Libros</h1>
+      </div>
+   );
+}
+
+Libros.propTypes = {};
+
+Libros.defaultProps = {};
+
+export default Libros;
