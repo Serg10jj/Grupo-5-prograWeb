b2da5c444b877d0e50c05879ed4efe72fda3ad77
Serg10jj <sergio.jimenez@ulatina.net>
2024-04-08 23:35:56 -0600

    DB Conection

--- back-end/server.js
+++ back-end/server.js
@@ -3,15 +3,11 @@ const cors = require("cors");
 const app = express();
 const port = 3008;
 const { Sequelize, DataTypes } = require("sequelize");
-
 app.use(cors());
 // Express middleware for parsing JSON
 app.use(express.json());
-
 //Conexion a la DB
-
 // Database connection
-
 const sequelize = new Sequelize({
   dialect: "mysql",
   host: "localhost",
@@ -25,13 +21,11 @@ class Entity {
     this.name = name;
     this.model = sequelize.define(name, fields);
   }
-
   async sync() {
     await this.model.sync({ force: true });
     console.log(`Table for ${this.name} synchronized`);
   }
 }
-
 // Define a simple schema for the User entity
 const userSchema = {
   user_id: {
@@ -61,7 +55,6 @@ const userSchema = {
     unique: false,
   },
 };
-
 const libroSchema = {
   libro_id: {
     type: DataTypes.INTEGER,
@@ -80,16 +73,13 @@ const libroSchema = {
     unique: false,
   },
 };
-
 // Create User entity using the schema
 const User = new Entity("User", userSchema);
 const Libro = new Entity("Libro", libroSchema);
-
 // Synchronize the database with the defined models.
 // This will create the tables if they do not exist
 // It will also create the tables with the defined schema
 // it will delete the information in the table
-
 const syncronizeDB = () => {
   sequelize
     .sync()
@@ -101,13 +91,10 @@ const syncronizeDB = () => {
       console.error("Error synchronizing database:", error);
     });
 };
-
  syncronizeDB();
-
 app.get("/api", (req, res) => {
   res.send("Hello World!");
 });
-
 
 app.post("/login", async (req, res) => {
   try {
@@ -120,7 +107,6 @@ app.post("/login", async (req, res) => {
         user_password: user_password,
       },
     });
-
     if (user) {
       res.status(200).json({ message: "Login successful", user });
     } else {
@@ -131,7 +117,6 @@ app.post("/login", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 app.post("/register", async (req, res) => {
   try {
     const { user_email, user_name, user_last_name, user_password } = req.body;
@@ -143,14 +128,12 @@ app.post("/register", async (req, res) => {
       user_last_name,
       user_password,
     });
-
     res.status(201).json({ message: "User created", user });
   } catch (error) {
     console.error("Error creating user:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 app.get("/users", async (req, res) => {
   try {
     const users = await User.model.findAll();
@@ -160,7 +143,6 @@ app.get("/users", async (req, res) => {
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 app.delete("/user/:user_id", async (req, res) => {
   try {
     const { user_id } = req.params;
@@ -169,14 +151,12 @@ app.delete("/user/:user_id", async (req, res) => {
         user_id,
       },
     });
-
     res.status(204).json({ message: "User deleted" });
   } catch (error) {
     console.error("Error deleting user:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
 });
-
 app.put("/users/:user_id", async (req, res) => {
   try {
     const { user_id } = req.params;
@@ -194,7 +174,6 @@ app.put("/users/:user_id", async (req, res) => {
         },
       }
     ); // Update the user with the given user_id
-
     res.status(204).json({ message: "User updated" });
   } catch (error) {
     console.error("Error updating user:", error);
@@ -204,4 +183,8 @@ app.put("/users/:user_id", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+
+
+
 
